f05f1110e472715059709c6de37dfba1ce520828
Burim <bkryeziu8@gmail.com>
2024-05-19 19:32:32 +0200

    unit test on button and link components

--- src/app/component/button/button.component.spec.ts
+++ src/app/component/button/button.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { ButtonComponent } from './button.component';
 
 describe('ButtonComponent', () => {
@@ -18,6 +18,42 @@ describe('ButtonComponent', () => {
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('should render correct variant', () => {
+    component.variant = 'success';
+
+    fixture.detectChanges();
+
+    const buttonElement = fixture.debugElement.query(By.css('button'));
+    const classList = buttonElement.nativeElement.classList;
+
+    console.log('Class list:', classList);
+
+    expect(classList).toContain('bg-success-500');
+    expect(classList).toContain('hover:bg-success-800');
+  });
+
+  it('should render correct content in button', () => {
+    // Set the component input
+    component.variant = 'success';
+    component.type = 'submit';
+
+    fixture.detectChanges();
+
+    const buttonElement = fixture.debugElement.query(By.css('button')).nativeElement;
+
+    const projectedContent = buttonElement.textContent.trim();
+
+    // ??????????????
+    // console.log('Projected Button Conent:', projectedContent);
+
+    // Expect the projected content to match the expected value
+    expect(projectedContent).toBe('');
+  });
+
+
+
 });
--- src/app/component/link/link.component.spec.ts
+++ src/app/component/link/link.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LinkComponent } from './link.component';
-import {ActivatedRoute} from "@angular/router";
+import { By } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 
 describe('LinkComponent', () => {
   let component: LinkComponent;
@@ -24,15 +24,66 @@ describe('LinkComponent', () => {
           }
         }
       ]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LinkComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('should render correct type', () => {
+    component.type = 'success';
+
+    fixture.detectChanges();
+
+    const linkElement = fixture.debugElement.query(By.css('a'));
+    const classList = linkElement.nativeElement.classList;
+
+    // Check Inspect inside testing browser to check the ->
+    // return data if its correct of what your asking to do 
+    // DETELE after checking if correct
+    console.log('Class list:', classList);
+
+    expect(classList).toContain('bg-success-500');
+    expect(classList).toContain('hover:bg-success-800');
+  });
+
+  it('should render correct content in link', () => {
+    
+    component.type = 'success';
+  
+    fixture.detectChanges();
+  
+    const linkElement = fixture.debugElement.query(By.css('a')).nativeElement;
+  
+    const linkContent = linkElement.textContent.trim();
+  
+    // howww???
+    console.log('Link Content:', linkContent);
+  
+    expect(linkContent).toBe('');
+  });
+  
+
+  
+
+  // it('should render correct content in link', () => {
+  //   // Set the component input
+  //   component.type = 'success';
+
+  //   // Trigger change detection to update the template
+  //   fixture.detectChanges();
+
+  //   // Query the anchor element and check its content
+  //   const linkElement = fixture.debugElement.query(By.css('a')).nativeElement;
+
+  //   expect(linkElement.textContent.trim()).toBe('Link'); // Adjust as per your actual link content
+  // });
+
+
+
 });
