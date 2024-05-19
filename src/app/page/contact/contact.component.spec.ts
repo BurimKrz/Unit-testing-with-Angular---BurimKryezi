18399ee70887ef3925b3287f321592d15945b5f4
Burim <bkryeziu8@gmail.com>
2024-05-19 22:58:56 +0200

    unit tests added

--- src/app/page/contact/contact.component.spec.ts
+++ src/app/page/contact/contact.component.spec.ts
@@ -1,17 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactComponent } from './contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { ButtonComponent } from '../../component/button/button.component';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
 
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ContactComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        ReactiveFormsModule,
+        ContactComponent,
+        ButtonComponent
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(ContactComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +28,21 @@ describe('ContactComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should disable the submit button when the form is invalid', () => {
+    const submitButton = fixture.debugElement.query(By.css('app-button button')).nativeElement;
+    expect(submitButton.disabled).toBeTrue();
+  });
+
+  it('should enable the submit button when the form is valid', () => {
+    component.contactForm.controls['name'].setValue('John Doe');
+    component.contactForm.controls['email'].setValue('john.doe@example.com');
+    component.contactForm.controls['phone'].setValue('1234567890');
+    component.contactForm.controls['comment'].setValue('This is a comment');
+    fixture.detectChanges();
+
+    const submitButton = fixture.debugElement.query(By.css('app-button button')).nativeElement;
+    expect(submitButton.disabled).toBeFalse();
   });
 });
--- src/app/service/user.service.spec.ts
+++ src/app/service/user.service.spec.ts
@@ -1,19 +1,38 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 import { UserService } from './user.service';
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { MockUserList } from '../mock/user';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientTestingModule],
+      providers: [UserService]
     });
     service = TestBed.inject(UserService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call the API and return user date', () => {
+    const expectedUrl = 'https://jsonplaceholder.typicode.com/users'; 
+    service.getUsers().subscribe((data) => {
+      console.log(data); 
+      expect(data).toEqual(MockUserList);
+    });
+    const req: TestRequest = httpTestingController.expectOne(expectedUrl);
+    expect(req.request.method).toEqual('GET');
+    
+    req.flush(MockUserList);
   });
 });
