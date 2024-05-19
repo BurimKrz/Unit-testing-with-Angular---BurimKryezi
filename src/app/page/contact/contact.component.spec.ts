b374d1fe9017297a0cea6408392fee397c540b97
Burim <bkryeziu8@gmail.com>
2024-05-19 23:07:33 +0200

    final commit

--- src/app/page/contact/contact.component.spec.ts
+++ src/app/page/contact/contact.component.spec.ts
@@ -1,15 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ContactComponent } from './contact.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
+import { ContactComponent } from './contact.component';
 import { ButtonComponent } from '../../component/button/button.component';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
 
-  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -44,5 +42,26 @@ describe('ContactComponent', () => {
 
     const submitButton = fixture.debugElement.query(By.css('app-button button')).nativeElement;
     expect(submitButton.disabled).toBeFalse();
+  });
+
+    // single tesing components
+  it('should render name input', () => {
+    const nameInput = fixture.debugElement.query(By.css('input[formControlName="name"]'));
+    expect(nameInput).toBeTruthy();
+  });
+
+  it('should render email input', () => {
+    const emailInput = fixture.debugElement.query(By.css('input[formControlName="email"]'));
+    expect(emailInput).toBeTruthy();
+  });
+
+  it('should render phone input', () => {
+    const phoneInput = fixture.debugElement.query(By.css('input[formControlName="phone"]'));
+    expect(phoneInput).toBeTruthy();
+  });
+
+  it('should render comment textarea', () => {
+    const commentTextarea = fixture.debugElement.query(By.css('textarea[formControlName="comment"]'));
+    expect(commentTextarea).toBeTruthy();
   });
 });
